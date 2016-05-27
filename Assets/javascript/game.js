5d402bc4ec99616319fb396a5482fd7ec29e73b4
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-05-27 13:55:02 -0400

    first jQuery elements

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,8 +1,19 @@
-var characters = ["WILL 'THE FRESH PRINCE' SMITH", "PHILLIP BANKS", "VIVIAN BANKS", "CARLTON BANKS", "HILLARY BANKS", "ASHLEY BANKS", "GEOFFREY", "NICKY BANKS", "JAZZ", "VY SMITH", "LISA WILKES", "JACKIE AMES"]
+var characters = ["WILL 'THE FRESH PRINCE' SMITH", "PHILLIP BANKS", 
+"VIVIAN BANKS", "CARLTON BANKS", "HILLARY BANKS", "ASHLEY BANKS", 
+"GEOFFREY", "NICKY BANKS", "JAZZ", "VY SMITH", "LISA WILKES", "JACKIE AMES"]
 var wins = 0;
 var validInput = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 function getRandomInt(max) {//Returns random integer between 0 and max-1.
     return Math.floor(Math.random() * max)
+}
+
+function alertBox(message){
+	return "<div class = 'alert alert-warning alert-dismissable' role='alert' id='alertDiv'>"
+	+ message + "</div"
+}
+
+String.prototype.replaceAt = function(index, character) {
+    return this.slice(0, index) + character + this.slice(index+character.length);
 }
 
 	function startGame(){
@@ -11,8 +22,8 @@ function getRandomInt(max) {//Returns random integer between 0 and max-1.
 		var displayCharacter = "";
 		var remainingGuesses = 10;
 		var alreadyGuessed = "";
-		var correct = 0;
 		
+		console.log(currentCharacter);
 		//Fills displayCharacter with the appropriate number of blanks and spaces.
 		for (var i = 0; i < currentCharacter.length; i++){
 			if (currentCharacter[i] === " "){
@@ -31,43 +42,64 @@ function getRandomInt(max) {//Returns random integer between 0 and max-1.
 		// Displays the letters that gave already been guessed.
 		document.getElementById("alreadyGuessed").innerHTML = alreadyGuessed;
 
+		//Displays total games won.
+		document.getElementById("gamesWon").innerHTML = wins;
+
 		document.onkeyup = function(event) {
 
+			var correct = 0;
+			console.log("correct:" + correct);
+
 			var userInput = String.fromCharCode(event.keyCode).toUpperCase();
+			console.log(userInput)
 
 
 			//Filters non-letter keys.
 			if (validInput.indexOf(userInput) === -1){
-				alert ("Invalid input!")
+				$("#alertDiv").html(alertBox("Invalid input, Homes!"))
+				console.log("Invalid")
 			}
 
 			//Filters letters that have already been guessed.
 			else if (alreadyGuessed.indexOf(userInput) !==-1 || displayCharacter.indexOf(userInput) !==-1){
-				alert("You already guessed that letter!")
+				alert("Yo, you already guessed that letter!");
+				console.log("Guessed")
 			}
 
 			else{
 				//Loops through currentCharacter to evaluate all valid guesses.
 				for (var i = 0; i < currentCharacter.length; i++){
 					if (userInput === currentCharacter[i]){
-						displayCharacter[i] = userInput;
+						displayCharacter = displayCharacter.replaceAt(i, userInput);
+						document.getElementById("game").innerHTML = displayCharacter;
 						correct ++;
 					}
 				}
+				console.log("loop")
+
 				//Handles incorrect guesses.
 				if (correct === 0){
+					//Updates remainingGuesses and its corresponding html panel
 					remainingGuesses --;
-					if (remainingGuesses === 0){return /*end game*/}
+					document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+					//Updates alreadyGuessed and its corresponding html panel
 					alreadyGuessed += userInput;
+					document.getElementById("alreadyGuessed").innerHTML = alreadyGuessed;
+					console.log(alreadyGuessed)
+
+					//Ends the game if the user is out of guesses
+					if (remainingGuesses < 1){return alert("Yo Homes, smell ya later!")}
 				}
 
 				if (displayCharacter === currentCharacter) {
-					alert("You got it!")
+					wins++;
+					return 	alert("You got it!");
 				}
+				console.log("correct:" + correct);
+				console.log(displayCharacter);
+				console.log("Remaining guesses:" + remainingGuesses)
 			}
 		}
 	}
 
-var gamesWon = "<p> Games won:" + wins + "</p>"
-document.getElementById("#gamesWon").innerHTML = gamesWon;
 
